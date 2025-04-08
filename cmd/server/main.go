3abc3fbfbd334a70f02a13f2bfa9750f62fc2654
Denis <denissscare@gmail.com>
2025-04-08 20:50:37 +0800

    Add test server to check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/denissscare/todo-go/internal/config"
+	"github.com/go-chi/chi/v5"
 )
 
 // TODO: Добавить логирование INFO
-func printConfig(cfg *config.Config) {
-	fmt.Printf("\n\nStorage path: %v\n", cfg.StoragePath)
+func startInfo(cfg *config.Config) {
+	fmt.Printf("\nСервер запущен по адресу: %v\n", cfg.HTTPServer.Address)
+
+	fmt.Print("\nПараметры конфига:")
+	fmt.Printf("Storage path: %v\n", cfg.StoragePath)
 	fmt.Printf("Address: %v\n", cfg.HTTPServer.Address)
 	fmt.Printf("Timeout: %v\n\n\n", cfg.HTTPServer.Timeout)
 }
 
 func main() {
 	var config = config.MustLoad()
-	printConfig(config)
+
+	router := chi.NewRouter()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print("INFO: GET /\n")
+		w.Write([]byte("Start page"))
+	})
+
+	server := &http.Server{
+		Addr:         config.Address,
+		Handler:      router,
+		ReadTimeout:  config.HTTPServer.Timeout,
+		WriteTimeout: config.HTTPServer.Timeout,
+		IdleTimeout:  config.HTTPServer.IdleTimeout,
+	}
+
+	startInfo(config)
+	if err := server.ListenAndServe(); err != nil {
+		//TODO: Добавить логирование ERROR
+		fmt.Printf("Ошибка запуска сервера: %s", err)
+	}
+
+	//TODO: Добавить логирование INFO
+	fmt.Print("Сервер остановлен")
 }
