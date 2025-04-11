03188dfa0c34db791b503ae53f6678e1f763ea9f
Denis <denissscare@gmail.com>
2025-04-11 18:56:42 +0800

    Add validator for request

--- internal/handlers/saveTodo/saveTodo.go
+++ internal/handlers/saveTodo/saveTodo.go
@@ -6,10 +6,11 @@ import (
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 )
 
 type Request struct {
-	Description string   `json:"description"`
+	Description string   `json:"description" validate:"required,min=1"`
 	Tags        []string `json:"tags,omitempty"`
 }
 
@@ -36,6 +37,7 @@ func New(log *slog.Logger, todoSaver TodoSaver) http.HandlerFunc {
 		)
 
 		var req Request
+
 		err := render.DecodeJSON(r.Body, &req)
 		if err != nil {
 			log.Error("Failed to decode request body", slog.String("error", err.Error()))
@@ -44,6 +46,12 @@ func New(log *slog.Logger, todoSaver TodoSaver) http.HandlerFunc {
 		}
 
 		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", slog.String("error", err.Error()))
+			render.JSON(w, r, Message{Status: "Error", Error: err.Error()})
+			return
+		}
 
 		id, err := todoSaver.AddTodo(req.Description, req.Tags)
 		if err != nil {
